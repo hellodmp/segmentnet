9bda29db7365c9133f860b419632130eb2715d5e
andrew <hello@163.com>
2017-01-11 05:07:34 -0800

    base function

---
+++ preprocess/Image.py
@@ -0,0 +1,101 @@
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+import matplotlib.pyplot as plt
+import SimpleITK as sitk
+from skimage import io
+from skimage import data
+from preprocess import dicomparser
+
+
+def get_imageData(ct_file):
+    ct = dicomparser.DicomParser(filename=ct_file)
+    imageData = ct.GetImageData()
+    return imageData
+
+def read_images(fileList):
+    image_dict = dict()
+    rescalFilt = sitk.RescaleIntensityImageFilter()
+    rescalFilt.SetOutputMaximum(1)
+    rescalFilt.SetOutputMinimum(0)
+    for path in fileList:
+        info = get_imageData(path)
+        image = rescalFilt.Execute(sitk.Cast(sitk.ReadImage(path), sitk.sitkFloat32))
+        image_dict[info["position"][2]] = sitk.GetArrayFromImage(image)
+    return image_dict
+
+
+def read_image(path):
+    #read dicom
+    data = sitk.ReadImage(path, sitk.sitkFloat32)
+    filter = sitk.RescaleIntensityImageFilter()
+    data = filter.Execute(data,0,1)
+    nda = sitk.GetArrayFromImage(data)
+    (d, w, h)=nda.shape
+    image = nda.reshape((d,w,h)).transpose(1, 2, 0)
+    return image
+
+
+def dict2vol(imageDict):
+    list = sorted(imageDict.iteritems(), key=lambda d: d[0])
+    (d, w, h) = list[0][1].shape
+    data = np.zeros((len(list), w, h))
+    image = sitk.GetImageFromArray(data)
+    return image
+
+def getNumpyData(self, dat, method, volSize, dstRes):
+    ret = dict()
+    for key in dat:
+        ret[key] = np.zeros([volSize[0], volSize[1], volSize[2]],
+                            dtype=np.float32)
+        img = dat[key]
+
+        # we rotate the image according to its transformation using the direction and according to the final spacing we want
+        factor = np.asarray(img.GetSpacing()) / [dstRes[0], dstRes[1], dstRes[2]]
+        factorSize = np.asarray(img.GetSize() * factor, dtype=float)
+        newSize = np.max([factorSize, volSize], axis=0)
+        newSize = newSize.astype(dtype=int)
+
+        T = sitk.AffineTransform(3)
+        T.SetMatrix(img.GetDirection())
+        resampler = sitk.ResampleImageFilter()
+        resampler.SetReferenceImage(img)
+        resampler.SetOutputSpacing([dstRes[0], dstRes[1], dstRes[2]])
+        resampler.SetSize(newSize)
+        resampler.SetInterpolator(method)
+        if self.params['normDir']:
+            resampler.SetTransform(T.GetInverse())
+
+        imgResampled = resampler.Execute(img)
+        imgCentroid = np.asarray(newSize, dtype=float) / 2.0
+        imgStartPx = (imgCentroid - volSize / 2.0).astype(dtype=int)
+        regionExtractor = sitk.RegionOfInterestImageFilter()
+        regionExtractor.SetSize(list(volSize.astype(dtype=int)))
+        regionExtractor.SetIndex(list(imgStartPx))
+        imgResampledCropped = regionExtractor.Execute(imgResampled)
+
+        ret[key] = np.transpose(sitk.GetArrayFromImage(imgResampledCropped).astype(dtype=float), [2, 1, 0])
+
+    return ret
+
+
+def sitk_show(nda, title=None, margin=0.0, dpi=40):
+    figsize = (1 + margin) * nda.shape[0] / dpi, (1 + margin) * nda.shape[1] / dpi
+
+    extent = (0, nda.shape[1], nda.shape[0], 0)
+    fig = plt.figure(figsize=figsize, dpi=dpi)
+    ax = fig.add_axes([margin, margin, 1 - 2 * margin, 1 - 2 * margin])
+    for k in range(0, nda.shape[2]):
+        print "printing slice " + str(k)
+        ax.imshow(np.squeeze(nda[:, :, k]),cmap ='gray', extent=extent, interpolation=None)
+        plt.draw()
+        #plt.pause(1)
+        plt.waitforbuttonpress()
+
+if __name__ == "__main__":
+    path = "../Dataset/V13265/"
+    ct_list = [path + f for f in listdir(path) if isfile(join(path, f)) and f.startswith('CT')]
+    images = read_images(ct_list)
+    image = dict2vol(images)
+    print image
---
+++ preprocess/Label.py
@@ -0,0 +1,88 @@
+from os import listdir
+from os.path import isfile, join
+
+import numpy as np
+from matplotlib.path import Path
+
+from preprocess import dicomparser
+
+
+def get_contours(structure_file, structure_name):
+    rtss = dicomparser.DicomParser(filename=structure_file)
+    structures = rtss.GetStructures()
+    for i, structure in structures.iteritems():
+        if structure['name'] == structure_name:
+            return structure['planes']
+    return {}
+
+def get_imageData(ct_file):
+    ct = dicomparser.DicomParser(filename=ct_file)
+    imageData = ct.GetImageData()
+    return imageData
+
+#convert old contours to new contours
+def contours_convert(contours_dict, imageData):
+    newContours = {}
+    for key in contours_dict.keys():
+        contours = contours_dict[key]
+        pixelspacing = imageData["pixelspacing"]
+        position = imageData["position"]
+        list = []
+        for contour in contours:
+            list.append([((item[0]-position[0])/pixelspacing[0],(item[1]-position[1])/pixelspacing[1]) for item in contour["contourData"]])
+        newContours[key] = list
+    return newContours
+
+def get_mask(lines, rows, columns):
+    mask_list = []
+    y_list = range(0,columns)
+    for line in lines:
+        mask = np.zeros((rows, columns))
+        polyline = Path(line)
+        for x in range(0,rows):
+            points = [(x,y) for y in y_list]
+            mask[x,:] = polyline.contains_points(points)
+        mask_list.append(mask)
+    for i in range(len(mask_list)):
+        if i == 0:
+            result_mask = mask_list[0]
+        else:
+            result_mask = np.logical_or(result_mask, mask_list[i])
+    return result_mask
+
+#convert new contours to mask
+def contours2mask(contours_dict, imageData):
+    mask_dict = {}
+    for key in contours_dict.keys():
+        contours = contours_dict[key]
+        mask = get_mask(contours,imageData["rows"],imageData["columns"])
+        mask_dict[key] = mask
+    return mask_dict
+
+def get_mask_dict(path,structure_names):
+    structure_file = [path+f for f in listdir(path) if isfile(join(path, f)) and f.startswith('RS')]
+    ct_list = [path+f for f in listdir(path) if isfile(join(path, f)) and f.startswith('CT')]
+    z_list = []
+    for path in ct_list:
+        imageData = get_imageData(path)
+        z_list.append(imageData["position"][2])
+    z_list.sort()
+
+    mask_list = []
+    for structure_name in structure_names:
+        contours = get_contours(structure_file[0], structure_name)
+        if len(contours) > 0:
+            new_contours = contours_convert(contours, imageData)
+            mask = contours2mask(new_contours, imageData)
+            mask_list.append(mask)
+    return z_list,mask_list
+
+
+if __name__ == '__main__':
+    z_list, mask_list = get_mask_dict("./Dataset/V13265/", ["PTV","Bladder","FemoralHead"])
+    print z_list, mask_list
+    for mask_dict in mask_list:
+        for key in mask_dict.keys():
+            mask = mask_dict[key]
+            print key, np.mean(mask)
+        print "\n"
