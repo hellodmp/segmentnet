1520df0275fb368035c73d9d001d72ee653dfbf6
dmp <hellodmp@163.com>
2017-02-11 21:26:27 +0800

    New changelist

--- ExportRT1.py
+++ ExportRT1.py
@@ -33,29 +33,54 @@ class RTExport(object):
         structureSet.ROINumber = roiNumber
         return structureSet
 
-    def modifyRoiObservation(self, roiObservation,roiNumber):
-        roiObservation.ReferenceROINumber = roiNumber
+    def modifyRoiObservation(self, roiObservation, roiNumber):
+        roiObservation.ObservationNumber = roiNumber
+        roiObservation.RefdROINumber = roiNumber
+        roiObservation.ReferencedROINumber = roiNumber
         return roiObservation
 
-    def modifyRoiContour(self, roiContour,roiNumber):
-        roiContour.ReferenceROINumber = roiNumber
+    #label_list=[(sopInstanceUID,[(x,y,z),(x,y,z)]),(sopInstanceUID,[(x,y,z),(x,y,z)])]
+    def modifyRoiContour(self, roiContour, roiNumber, label_list):
+        roiContour.RefdROINumber = roiNumber
+        roiContour.ReferencedROINumber = roiNumber
+        contour = roiContour.Contours[0]
+        contour_list = []
+        for label in label_list:
+            (sopInstanceUID, points_list) = label
+            contour_temp = copy.deepcopy(contour)
+            contour_temp.ContourImages[0].ReferenedSOPInstanceUID = sopInstanceUID
+            contour_temp.NumberofContourPoints = len(points_list)
+            contour_temp.ContourData = []
+            contour_data = []
+            for item in points_list:
+                contour_data.append(item[0])
+                contour_data.append(item[1])
+                contour_data.append(item[2])
+            contour_temp.ContourData = contour_data
+            contour_list.append(contour_temp)
+        roiContour.Contours = contour_list
         return roiContour
 
-    def addNewLabel(self, name):
+    def addNewLabel(self, name, label_list):
+        roiNumber = 0
+        for structureSet in self.destDs.StructureSetROIs:
+            if structureSet.ROINumber > roiNumber:
+                roiNumber = structureSet.ROINumber
+        roiNumber += 1
         structureSet, roiObservation, roiContour = self.get_by_structure(name)
-        roiId = len(self.destDs.StructureSetROIs)
-        structureSet = self.modifyStructureSet(structureSet,roiId)
+
+        structureSet = self.modifyStructureSet(structureSet,roiNumber)
         structureSetList = self.destDs.StructureSetROIs
         structureSetList.append(structureSet)
 
-        roiObservation = self.modifyRoiObservation(roiObservation,roiId)
+        roiObservation = self.modifyRoiObservation(roiObservation,roiNumber)
         roiObservationList = self.destDs.RTROIObservations
         roiObservationList.append(roiObservation)
 
-        roiContour = self.modifyRoiContour(roiContour,roiId)
+        roiContour = self.modifyRoiContour(roiContour,roiNumber,label_list)
         roiContourList = self.destDs.ROIContours
         roiContourList.append(roiContour)
-        print "ok"
+        print roiContour
 
     def save(self):
         self.destDs.save_as(self.destPath)
@@ -71,10 +96,12 @@ if __name__ == "__main__":
 '''
 
 if __name__ == "__main__":
-    source = "Dataset/data/V13195/RS.1.2.246.352.71.4.126422491061.189601.20151221094515.dcm"
-    dest = "Dataset/data/test.dcm"
+    source = "Dataset/data1/RS.1.2.246.352.71.4.126422491061.189601.20151221094515.dcm"
+    dest = "Dataset/data1/test.dcm"
     rtExport = RTExport(source, dest)
     #print rtExport.structureSetList
     name = "Urinary Bladder"
-    rtExport.addNewLabel(name)
+    label_list = [("1.3.12.2.1107.5.1.4.49611.30000014052605590490600002095",[(-45,-234,-381.0),(20,-234,-381.0),(20,-200,-381.0),(-45,-200,-381.0)]),
+                  ("1.3.12.2.1107.5.1.4.49611.30000014052605590490600002096",[(-45,-234,-386.0),(20,-234,-386.0),(20,-200,-386.0),(-45,-200,-386.0)])]
+    rtExport.addNewLabel(name,label_list)
     rtExport.save()
